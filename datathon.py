b2b5164e93f014821143f50bb1dd8a12eabdf1c8
halbelajar <74313956+halbelajar@users.noreply.github.com>
2020-12-01 09:00:21 -0500

    first commit

---
+++ datathon.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+
+csv_file_total = ("data/master.csv")
+df= pd.read_csv(csv_file_total)
+
+st.title("Weekly Total Spending")
+
+#Sidebar 
+st.sidebar.title("About")
+st.sidebar.markdown("The dashboard is a visualization of data from Price Intelligence database." )
+
+st.sidebar.markdown("For further information, please visit: datathon.dosm.gov.my")
+st.sidebar.text("Developed by: Anas, Usamah, Zahir")
+st.sidebar.text("Group: Hargakini")
+
+#Create the selection box
+selected_metrics = st.selectbox(
+    label="Pick your category", options=['Air Conditioner','Bedroom Furniture','Cooktops','Freezer','Home Furniture','Living Furniture','Laptops','Phones','Refrigerator','Smart Televisions'])
+st.write("You selected: ", selected_metrics)
+
+# Create traces
+fig = go.Figure()
+if selected_metrics == 'Air Conditioner':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.air_con,
+                    mode='markers',
+                    name='Air Conditioner'))
+if selected_metrics == 'Bedroom Furniture':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.bedroom,
+	                    mode='markers', name='Bedroom'))
+if selected_metrics == 'Cooktops':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.cooktops,
+	                    mode='markers', name='Cooktops'))
+if selected_metrics == 'Freezer':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.freezer,
+	                    mode='markers',
+	                    name='Freezer'))
+if selected_metrics == 'Home Furniture':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.home,
+	                    mode='markers', name='Home Furniture'))
+if selected_metrics == 'Living Furniture':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.living,
+	                    mode='markers', name='Living'))
+if selected_metrics == 'Laptops':
+    fig.add_trace(go.Scatter(x=df.Date, y=df.laptops,
+                         mode='markers',
+                         name='Laptops'))
+if selected_metrics == 'Phones':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.phones,
+	                    mode='markers', name='Phones'))
+if selected_metrics == 'Refrigerator':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.refrigerator,
+	                    mode='markers', name='Refrigerator'))
+if selected_metrics == 'Smart Televisions':
+	fig.add_trace(go.Scatter(x=df.Date, y=df.tv,
+	                    mode='markers', name='Cooktops'))
+st.plotly_chart(fig, use_container_width=True)
+
+st.markdown("Note: Hover over the scatter plot to see the figure.")
